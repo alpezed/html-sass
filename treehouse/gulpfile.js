d3f7576f3d33b86981bc594ed3c59c7d927edb7c
alpezed <alpezed@gmail.com>
2018-03-19 13:38:26 +0800

    Updaed gulp.js file

--- treehouse/gulpfile.js
+++ treehouse/gulpfile.js
@@ -102,8 +102,8 @@ gulp.task('build', ['sass','html','style','scripts','images', 'fonts']);
 
 gulp.task('watch', ['browserSync'], function() {
     gulp.watch(path.app.style, ['sass', 'style']);
-    gulp.watch(path.app.html, ['html']);
     gulp.watch(path.app.js, ['scripts']);
+    gulp.watch(path.app.html, ['html']);
     gulp.watch(path.app.img, ['images']);
     gulp.watch(path.app.fonts, ['fonts']);
 });
